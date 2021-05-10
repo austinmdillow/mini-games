dedebf84b02ea4f00b01a548abb568cf51c5b3b5
austinmdillow <austinmdillow@gmail.com>
2021-05-10 11:36:10 -0400

    changed iffy example folder names so build passes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ##### build #####
 
-find . -iname "*.lua" | xargs luac -p || { echo 'luac parse test failed' ; exit 1; }
+find . -iname "*.lua" -type f | xargs luac -p || { echo 'luac parse test failed' ; exit 1; }
 echo 'build passed'
